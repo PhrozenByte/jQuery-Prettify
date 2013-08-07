542eaf832480c88c08a0743f8731a0aa163996d9
Daniel Rudolf <daniel.rudolf@eifel-online.com>
2013-08-07 04:13:49 +0200

    Switched to LGPL v3 only

--- README.md
+++ README.md
@@ -32,6 +32,6 @@ License & Copyright
 -------------------
 Copyright (C) 2012-2013  Daniel Rudolf <http://www.daniel-rudolf.de/>
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3 of the License only.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, version 3 of the License only.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the [GNU General Public License](../blob/master/LICENSE.md) for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the [GNU Lesser General Public License](../blob/master/LICENSE) for more details.
--- jquery.prettify.js
+++ jquery.prettify.js
@@ -8,22 +8,24 @@
  * 	Copyright (C) 2012-2013  Daniel Rudolf <http://www.daniel-rudolf.de/>
  * 	
  * 	This program is free software: you can redistribute it and/or modify
- * 	it under the terms of the GNU General Public License as published by
- * 	the Free Software Foundation, version 3 of the License only.
+ * 	it under the terms of the GNU Lesser General Public License as
+ * 	published by the Free Software Foundation, version 3 of the License
+ * 	only.
  * 	
  * 	This program is distributed in the hope that it will be useful,
  * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
  * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * 	GNU General Public License for more details.
+ * 	GNU Lesser General Public License for more details.
  * 	
- * 	You should have received a copy of the GNU General Public License
- * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 	You should have received a copy of the GNU Lesser General Public
+ * 	License along with this program. If not, see
+ * 	<http://www.gnu.org/licenses/>.
  * 
  * @author	Daniel Rudolf
  * @copyright	2012-2013 Daniel Rudolf
  * 		<http://www.daniel-rudolf.de/>
- * @license	GNU GNU General Public License v3 only
- * 		<http://www.gnu.org/licenses/gpl-3.0.html>
+ * @license	GNU Lesser General Public License v3 only
+ * 		<http://www.gnu.org/licenses/lgpl-3.0.html>
  */
 (function($) {
 	// {{{ public API
